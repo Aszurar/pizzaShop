8874e1ae54b38334683ab340ede75fdf5e01377a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-29 21:19:27 -0300

    :sparkles: Add Dashboard and Orders pages :sparkles:

---
+++ src/pages/app/Orders/index.tsx
@@ -0,0 +1,53 @@
+import { Helmet } from 'react-helmet-async'
+
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrdersTableFilters } from './OrdersTableFilters'
+import { OrdersTablePagination } from './OrdersTablePagination'
+import { OrdersTableRow } from './OrdersTableRow'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+
+      <div className="flex flex-1 flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+
+        <div className="space-y-2.5">
+          <OrdersTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-35">Identificador</TableHead>
+                  <TableHead className="w-45">Realizado há</TableHead>
+                  <TableHead className="w-35">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-35">Total do pedido</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrdersTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <OrdersTablePagination pageIndex={0} perPage={10} totalCount={105} />
+        </div>
+      </div>
+    </>
+  )
+}
