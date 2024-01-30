c72a452205a43255dc882856f586178ed9930268
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-29 21:20:43 -0300

    :sparkles: Add App component with necessary imports and providers :sparkles:

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider } from './components/Theme/ThemeProvider'
+import { queryClient } from './lib/react-query'
+import { router } from './router'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
+  )
+}
