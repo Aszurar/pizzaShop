adb51c486edecbc2354f6c5ec7f20dccb1225907
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-22 11:51:09 -0300

    ✨ Adição de suporte para estado de carregamento nos botões e validação de entrada nos componentes de input e select✨

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,21 +4,33 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
+import { Spinner } from '../Spinner'
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+  cn(
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm',
+    'font-medium ring-offset-background transition-colors focus-visible:outline-none',
+    ' focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+    'disabled:cursor-not-allowed disabled:opacity-50',
+    'gap-2 duration-300',
+    'data-[loading=true]:cursor-wait data-[loading=true]:opacity-50',
+  ),
+
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'bg-primary text-primary-foreground hover:bg-primary/90 active:bg-primary disabled:hover:bg-primary data-[loading=true]:hover:bg-primary',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90 active:bg-destructive disabled:hover:bg-destructive data-[loading=true]:hover:bg-destructive',
         success:
-          'bg-emerald-500 text-black hover:bg-emerald-400 dark:bg-emerald-600 dark:hover:bg-emerald-500',
+          'bg-emerald-500 text-black hover:bg-emerald-400 dark:bg-emerald-600 dark:hover:bg-emerald-500 active:bg-emerald-500 dark:active:bg-emerald-600 disabled:hover:bg-emerald-500 dark:disabled:hover:bg-emerald-600 data-[loading=true]:hover:bg-emerald-500 dark:data-[loading=true]:hover:bg-emerald-600',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground disabled:hover:bg-background',
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground disabled:hover:bg-background active:bg-background disabled:hover:text-foreground disabled:hover:bg-background data-[loading=true]:hover:bg-background data-[loading=true]:hover:text-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80 active:bg-secondary disabled:hover:bg-secondary data-[loading=true]:hover:bg-secondary',
+        ghost:
+          'hover:bg-accent hover:text-accent-foreground active:bg-transparent',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
@@ -40,17 +52,51 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading = false,
+      onClick,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button'
+    const content = isLoading ? (
+      <>
+        <Spinner color={variant} /> Carregando...
+      </>
+    ) : (
+      children
+    )
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (isLoading) {
+        e.preventDefault()
+        return
+      }
+
+      onClick?.(e)
+    }
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={handleClick}
+        data-loading={isLoading}
         {...props}
-      />
+      >
+        {content}
+      </Comp>
     )
   },
 )
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,33 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  isInvalid?: boolean
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, isInvalid = false, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2',
+          'text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm',
+          'file:font-medium placeholder:text-muted-foreground focus-visible:outline-none',
+          'focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[invalid=true]:border-destructive',
+          className,
         )}
         ref={ref}
+        data-invalid={isInvalid}
         {...props}
       />
     )
-  }
+  },
 )
-Input.displayName = "Input"
+Input.displayName = 'Input'
 
 export { Input }
