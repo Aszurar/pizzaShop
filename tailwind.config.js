85b27759541584e2433544649265a261699705a2
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-22 11:50:34 -0300

    ✨ Adição de animação de spinner no Tailwind CSS para melhorar a experiência do usuário✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,10 +72,19 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        spinner: {
+          '0%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0.15',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        spinner: 'spinner 1.2s linear infinite',
       },
     },
   },
